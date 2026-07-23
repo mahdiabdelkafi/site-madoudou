1b0c14e95e6c410ded46fa9778573023229dc86b
Mahdi <mahdi.abdelkafi@gmail.com>
2026-07-23 12:59:51 +0200

    chore: remove unused tailwind vite import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import { tanstackRouter } from "@tanstack/router-plugin/vite"
-import tailwindcss from "@tailwindcss/vite";
+//import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   css: {
